79b0e7b7cbfaf95fb655403a5da728704e6b09f1
janclod <ctiecher@gmail.com>
2021-01-27 20:44:53 +0100

    Fixing Win lintr issue

--- R/app_server.R
+++ R/app_server.R
@@ -1,3 +1,9 @@
+#' Bundel server functions
+#' @noRd
+server_functions <- function() {
+  mod_dropdown_server("1")
+}
+
 #' The application server-side
 #'
 #' @param input,output,session Internal parameters for {shiny}.
@@ -6,5 +12,5 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # List the first level callModules here
-  mod_dropdown_server("1")
+  server_functions()
 }
