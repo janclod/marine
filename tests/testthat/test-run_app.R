4b11a62259cb15586f99ec97813577d9f992d52e
janclod <ctiecher@gmail.com>
2021-01-31 12:23:27 +0100

    Shinytest does not compare snapshots

--- tests/testthat/test-run_app.R
+++ tests/testthat/test-run_app.R
@@ -4,6 +4,8 @@ test_that("app works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
 
-  # Allow for the geenration for platform specific snapshots
+  # Allow for the generation for platform specific snapshots using suffix param
+  # 
   expect_pass(testApp("apps/run_app/",
-                      suffix = strsplit(utils::osVersion, " ")[[1]][1]))})
+                      compareImages = FALSE,
+                      suffix = strsplit(utils::osVersion, " ")[[1]][1]))},)
